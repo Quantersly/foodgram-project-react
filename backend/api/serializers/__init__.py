5c772d176b2e862c209ce70c9631cae7428a827e
Quantersly <loskutnikov.alexey@gmail.com>
2024-02-29 21:35:43 +0300

    diplom-4

--- backend/api/serializers/__init__.py
+++ backend/api/serializers/__init__.py
@@ -5,7 +5,9 @@ from .ingredient_in_recipe_serializer import (
 from .ingredient_serializer import IngredientSerializer
 from .recipe_read_serializer import RecipeReadSerializer
 from .recipe_create_serializer import RecipeCreateSerializer
-from .list_of_following_recipes_serializer import ListOfFollowingRecipesSerializer
+from .list_of_following_recipes_serializer import (
+    ListOfFollowingRecipesSerializer
+)
 from .tag_serializer import TagSerializer
 from .user_serializer import UserSerializer
 
