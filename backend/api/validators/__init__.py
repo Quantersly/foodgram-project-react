2014f28e311d082f49c77fbf56fd168844e5e229
Quantersly <loskutnikov.alexey@gmail.com>
2024-03-06 00:03:28 +0300

    15

--- backend/api/validators/__init__.py
+++ backend/api/validators/__init__.py
@@ -2,6 +2,6 @@ from .validator_username import validate_username
 from .validator_year import validate_year
 
 __all__ = (
-    validator_username,
-    validator_year
+    validate_username,
+    validate_year
 )
