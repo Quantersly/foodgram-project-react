7c853d367a3a208fed94833a2dee6cf980d522fd
Quantersly <loskutnikov.alexey@gmail.com>
2024-03-09 20:11:39 +0300

    7

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -5,11 +5,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(
-        'api/docs/',
-        TemplateView.as_view(
-            template_name='redoc.html',
-        ),
-    ),
     path('api/', include('api.urls')),
 ]
